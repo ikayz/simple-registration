1631a5c99e7dcdd320fd88026fce1316784ba9b5
Isaac Miti <ikayz@live.com>
2020-04-13 18:40:05 +0200

    Add index with extra fields

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+</head>
+<body>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <table>
+      <tr>
+        <td>Name: </td>
+        <td><input type="text" name="name"/></td>
+      </tr>
+      <tr>
+        <td>Email: </td>
+        <td><input type="email" name="email"/></td>
+      </tr>
+      <tr>
+        <td>Subject: </td>
+        <td><input type="text" name="subject"/></td>
+      </tr>
+      <tr>
+        <td>Comments: </td>
+        <td><textarea name="comments"></textarea></td>
+      </tr>
+      <tr>
+        <td>Password: </td>
+        <td><input type="password" name="password"/></td>
+      </tr>
+      <tr>
+        <td><input type="hidden" value="yes" name="validate"></td>
+        <td><input type="submit" name="submit"></td>
+      </tr>
+    </table>
+  </form>
+</body>
+</html>
+<?php
+  if (isset($_POST['validate'])) {
+    echo "Username: {$_POST['login_email']}<br>";
+    echo "Password: {$_POST['login_password']}";
+  } else {
+    echo "Did not receive any request!";
+  }
+?>
