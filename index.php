67483aaca247e538ffb7d5c1f032bc6dc9e6e072
Isaac Miti <ikayz@live.com>
2020-04-18 14:48:48 +0200

    Add front to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include'conn.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Retrieving Data from MySQL</title>
+</head>
+<body>
+
+</body>
+</html>
