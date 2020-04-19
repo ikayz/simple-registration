1f1a9ae0bf9bfbebfabb604a5f1baeadd9ff63bf
Isaac Miti <ikayz@live.com>
2020-04-19 14:09:34 +0200

    Add code to retrieve remaining columns from database

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>Retrieving Data from MySQL</title>
 </head>
 <body>
-  <table>
+  <table border="1">
     <thead>
       <th>ID</th>
       <th>Name</th>
@@ -21,7 +21,15 @@
           $sql = "SELECT * FROM comments";
           $run_sql = mysqli_query($conn, $sql);
           while($rows = mysqli_fetch_assoc($run_sql)) {
-            echo $rows['comments'];
+            echo '<tr>
+                    <td>' . $rows['id'] . '</td>
+                    <td>' . $rows['name'] . '</td>
+                    <td>' . $rows['email_address'] . '</td>
+                    <td>' . $rows['skill1'] . ',' . $rows['skill2'] . ',' . $rows['skill3'] . ',' .  $rows['skill4'] . '</td>
+                    <td>' . $rows['gender'] . '</td>
+                    <td>' . $rows['country'] . '</td>
+                  </tr>
+            ';
           }
       ?>
     </tbody>
