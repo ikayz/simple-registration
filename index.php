db1fd0778632ed49a689f6da6e37d385a1eef631
Isaac Miti <ikayz@live.com>
2020-04-19 13:49:22 +0200

    Add table

--- index.php
+++ index.php
@@ -7,12 +7,24 @@
   <title>Retrieving Data from MySQL</title>
 </head>
 <body>
-  <?php
-    $sql = "SELECT * FROM comments";
-    $run_sql = mysqli_query($conn, $sql);
-    while($rows = mysqli_fetch_assoc($run_sql)) {
-      echo $rows['comments'];
-    }
-  ?>
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Skills</th>
+      <th>Gender</th>
+      <th>Country</th>
+    </thead>
+    <tbody>
+      <?php
+          $sql = "SELECT * FROM comments";
+          $run_sql = mysqli_query($conn, $sql);
+          while($rows = mysqli_fetch_assoc($run_sql)) {
+            echo $rows['comments'];
+          }
+      ?>
+    </tbody>
+  </table>
 </body>
 </html>
