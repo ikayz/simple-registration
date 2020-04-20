b67c49d728fa3a87b3f97056ec4fa180bec56f06
Isaac Miti <ikayz@live.com>
2020-04-21 01:07:15 +0200

    Add required attribute to form elements

--- other_form.php
+++ other_form.php
@@ -6,26 +6,26 @@
     <div class="form-group row">
       <label for="name" class="control-label col-sm-1 text-right">Name</label>
       <div class="col-sm-3">
-        <input type="text" name="" id="name" class="form-control" placeholder="Type name" aria-describedby="helpId">
+        <input type="text" name="" id="name" class="form-control" placeholder="Type name" required>
       </div>
     </div>
     <div class="form-group row">
       <label for="email" class="control-label col-sm-1 text-right">Email</label>
       <div class="col-sm-3">
-        <input type="email" name="" id="email" class="form-control" placeholder="example@someone.com" aria-describedby="helpId">
+        <input type="email" name="" id="email" class="form-control" placeholder="example@someone.com" required>
       </div>
     </div>
     <div class="form-group row">
       <label for="subject" class="control-label col-sm-1 text-right">Subject</label>
       <div class="col-sm-3">
-        <input type="text" name="" id="subject" class="form-control" placeholder="Your subject" aria-describedby="helpId">
+        <input type="text" name="" id="subject" class="form-control" placeholder="Your subject" required>
       </div>
     </div>
     <div class="form-group row">
       <label for="gender" class="control-label col-sm-1 text-right">Gender</label>
       <div class="col-sm-3">
-        <select class="form-control" name="" id="gender">
-          <option value="selected">-- Select Gender --</option>
+        <select class="form-control" name="" id="gender" required>
+          <option value="">-- Select Gender --</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
@@ -34,17 +34,17 @@
     <div class="form-group row">
       <label for="subject" class="control-label col-sm-1 text-right">Skills</label>
       <div class="col-sm-6">
-        <label><input type="checkbox" name="" id="skills" class="checkbox-inline" aria-describedby="helpId"> JavaScript</label>
-        <label><input type="checkbox" name="" id="skills" class="checkbox-inline" aria-describedby="helpId"> Node JS</label>
-        <label><input type="checkbox" name="" id="skills" class="checkbox-inline" aria-describedby="helpId"> React</label>
-        <label><input type="checkbox" name="" id="skills" class="checkbox-inline" aria-describedby="helpId"> PHP</label>
+        <label><input type="checkbox" name="" id="skills" class="checkbox-inline"> JavaScript</label>
+        <label><input type="checkbox" name="" id="skills" class="checkbox-inline"> Node JS</label>
+        <label><input type="checkbox" name="" id="skills" class="checkbox-inline"> React</label>
+        <label><input type="checkbox" name="" id="skills" class="checkbox-inline"> PHP</label>
       </div>
     </div>
     <div class="form-group row">
       <label for="gender" class="control-label col-sm-1 text-right">Country</label>
       <div class="col-sm-3">
-        <select class="form-control" name="" id="gender">
-          <option value="selected">-- Select Country --</option>
+        <select class="form-control" name="" id="gender" required>
+          <option value="">-- Select Country --</option>
           <option value="angola">Angola</option>
           <option value="botswana">Botswana</option>
           <option value="canada">Cananda</option>
@@ -61,7 +61,7 @@
     <div class="form-group row">
       <label for="" class="control-label col-sm-1 text-right"></label>
       <div class="col-sm-3">
-        <button type="button" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
   </form>
