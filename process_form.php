b7cf8481f49cf28ba23084267b981dc6542fc6ed
Isaac Miti <ikayz@live.com>
2020-04-21 13:11:15 +0200

    Add insert query

--- process_form.php
+++ process_form.php
@@ -1,4 +1,6 @@
 <?php
+  include'includes/conn.php';
+
   if (isset($_POST['submit'])) {
       $name = trim(htmlspecialchars($_POST['name']));
       $email = strip_tags(trim($_POST['email']));
@@ -7,7 +9,11 @@
       $country = strip_tags($_POST['country']);
       $comments = trim(htmlspecialchars($_POST['comments']));
 
-
+      $insert_sql  = "INSERT INTO comments ";
+      $insert_sql .= "('name','email_address','subject','gender'";
+      $insert_sql .= ",'skill1','skill2','skill3','skill4','country','comments')";
+      $insert_sql .= "VALUES ('$name','$email','$subject','$gender'";
+      $insert_sql .= ",'$_POST[skill1],'$_POST[skill2]','$_POST[skill3]','$_POST[skill4]','$country','$comments')";
   } else {
     # code...
   }
