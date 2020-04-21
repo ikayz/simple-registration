3bd0be7d4cf8d51835bb40b93eab9a184fe708d7
Isaac Miti <ikayz@live.com>
2020-04-21 12:31:06 +0200

    Add form variables

--- process_form.php
+++ process_form.php
@@ -1,5 +1,12 @@
 <?php
   if (isset($_POST['submit'])) {
+      $name = trim(htmlspecialchars($_POST['name']));
+      $email = strip_tags(trim($_POST['email']));
+      $subject = htmlspecialchars($_POST['subject']);
+      $gender = strip_tags($_POST['gender']);
+      $country = strip_tags($_POST['country']);
+      $comments = trim(htmlspecialchars($_POST['comments']));
+
 
   } else {
     # code...
