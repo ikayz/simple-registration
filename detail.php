ef2774b31d9e66cb5c4987a50ff342a806d24de2
Isaac Miti <ikayz@live.com>
2020-04-21 19:37:41 +0200

    Add detail page

---
+++ detail.php
@@ -0,0 +1,9 @@
+<?php include'includes/conn.php';?>
+<?php include'includes/header.php';?>
+<body>
+  <div class="container">
+    <div class="jumbotron">
+
+    </div>
+  </div>
+<?php include'includes/footer.php'; ?>
